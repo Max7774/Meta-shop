a58354de304fbab34f7f99b478ed8179527a1035
Max7774 <mega_ymbetov@mail.ru>
2024-09-26 16:12:25 +0300

    [PWA]

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -20,13 +20,13 @@ export default () => {
           theme_color: "#ffffff",
           icons: [
             {
-              src: "/images/favicon-16x16.png",
-              sizes: "16x16",
+              src: "/images/android-chrome-192x192.png",
+              sizes: "192x192",
               type: "image/png",
             },
             {
-              src: "/images/favicon-32x32.png",
-              sizes: "32x32",
+              src: "/images/android-chrome-512x512.png",
+              sizes: "512x512",
               type: "image/png",
             },
           ],
