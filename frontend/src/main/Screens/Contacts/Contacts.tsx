106afc7f8034aab3a28ef92cb947c7a91abc2655
Max7774 <mega_ymbetov@mail.ru>
2024-09-27 18:20:04 +0300

    [SEO] - Fix

--- frontend/src/main/Screens/Contacts/Contacts.tsx
+++ frontend/src/main/Screens/Contacts/Contacts.tsx
@@ -2,6 +2,7 @@ import { Helmet } from "react-helmet-async";
 import { FaPhoneAlt, FaEnvelope, FaClock } from "react-icons/fa";
 import styles from "./Contacts.module.scss";
 import { Divider } from "@nextui-org/react";
+import Heading from "@UI/Heading";
 
 const Contacts = () => {
   return (
@@ -18,7 +19,7 @@ const Contacts = () => {
         />
       </Helmet>
 
-      <h1 className={styles.title}>Свяжитесь с нами</h1>
+      <Heading>Свяжитесь с нами</Heading>
 
       <div className="flex flex-wrap gap-3">
         <div className={styles["contact-item"]}>
@@ -40,18 +41,18 @@ const Contacts = () => {
             </p>
           </div>
         </div>
+      </div>
 
-        <div className={styles["contact-item"]}>
-          <FaClock className={styles.icon} />
-          <div>
-            <h3>Часы работы</h3>
-            <p>Пн-Вс:</p>
-            <p>Принимаются заказы: 9:00 — 13:00</p>
-            <p className="text-warning">Ягоды до 9:00</p>
-            <Divider className="my-1" />
-            <p>Доставка: 13:00 — 20:00</p>
-            <p className="text-warning">Ягоды до 13:00</p>
-          </div>
+      <div className="flex flex-row m-[10px] mt-7 sm:mt-20">
+        <FaClock className={styles.icon} />
+        <div>
+          <h3>Часы работы</h3>
+          <p>Пн-Вс:</p>
+          <p>Принимаются заказы: 9:00 — 13:00</p>
+          <p className="text-warning">Ягоды до 9:00</p>
+          <Divider className="my-1" />
+          <p>Доставка: 13:00 — 20:00</p>
+          <p className="text-warning">Ягоды до 13:00</p>
         </div>
       </div>
     </div>
